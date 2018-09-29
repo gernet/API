4b50e36da36a8bbf9cef4839a86b54a7bdf4298c
realsubin97 <38903619+realsubin97@users.noreply.github.com>
2018-09-29 15:55:49 +0900

    openshift ver2

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ from app import app
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
+    import threading
+    import pandas as pd
+    from bs4 import BeautifulSoup
+    import requests
