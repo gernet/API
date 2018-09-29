514cef4516dbcf4f00d8bd544d1eb56043310bef
realsubin97 <38903619+realsubin97@users.noreply.github.com>
2018-09-29 18:53:10 +0900

    openshift

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name ="APIfirebase",
+    version ="0.1",
+    packages= find_packages(),
+    install_requires = ['requests', 'pandas','bs4'],
+
+)
