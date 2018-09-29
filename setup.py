a569d2803e112fa39e2c19625f9136ddcb026231
realsubin97 <38903619+realsubin97@users.noreply.github.com>
2018-09-29 20:35:05 +0900

    pandas --> numpy, import delete, app install

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name ="APIfirebase",
     version ="1.0",
     packages= find_packages(),
-    install_requires = ['requests', 'numpy','bs4'],
+    install_requires = ['requests', 'numpy','bs4','app'],
 )
