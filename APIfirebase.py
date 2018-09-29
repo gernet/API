67496a4d9fcdad6f366496981032dc36e5c5e706
LNH <nan2320@tong.dongyang.ac.kr>
2018-09-29 15:53:03 +0900

    new

--- APIfirebase.py
+++ APIfirebase.py
@@ -5,16 +5,9 @@
 
 
 import threading
-
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-
-
-# In[ ]:
-
-
-## firebase와의 연동
 
 
 # In[2]:
@@ -31,14 +24,7 @@ cred = credentials.Certificate('realace2018-firebase-adminsdk-r7gk1-121be5edaf.j
 app=firebase_admin.initialize_app(cred, {
    'databaseURL': 'https://realace2018.firebaseio.com/'
 })
-
 ref=db.reference()
-
-
-# In[ ]:
-
-
-## 
 
 
 # In[3]:
@@ -49,14 +35,7 @@ numOfRows=[25, 16, 8, 9, 5, 5, 5, 31, 6, 7, 14, 12, 9, 1, 9, 9, 3]
 sidoName=['서울', '부산', '대구', '인천', '광주', '대전', '울산', '경기', '강원', '충북', '충남', '전북', '전남', '세종', '경북', '경남', '제주']
 
 
-# In[ ]:
-
-
-## firebase저장 함수
-
-
-# In[9]:
-
+# In[33]:
 
 
 def func():
@@ -70,10 +49,20 @@ def func():
         citylist=[]
         pm10list=[]
         timelist=[]
+        so2list=[]
+        colist=[]
+        o3list=[]
+        no2list=[]
+        
 
         datatime=soup.find_all('datatime')
         cityname=soup.find_all('cityname')
+        so2value=soup.find_all('so2value')
+        covalue=soup.find_all('covalue')
+        o3value=soup.find_all('o3value')
+        no2value=soup.find_all('no2value')
         pm10vale=soup.find_all('pm10value')
+        
 
         for code in datatime:
             timelist.append(code.text)
@@ -81,12 +70,24 @@ def func():
             citylist.append(code.text)
         for code in pm10vale:
             pm10list.append(code.text)
+        for code in so2value:
+            so2list.append(code.text)
+        for code in covalue:
+            colist.append(code.text)
+        for code in o3value:
+            o3list.append(code.text)
+        for code in no2value:
+            no2list.append(code.text)
+            
+    
+    
     
         numb=numOfRows[number]
     
         for num in range(0,numb):
-            users_ref= ref.child('pm')
-            users_ref.push({'sidoname':sidoName[number],'cityname':citylist[int(num)], 'pm10vale':pm10list[int(num)],'datatime':timelist[int(num)]})
+            users_ref= ref.child('air')
+            users_ref.push({'sidoname':sidoName[number],'cityname':citylist[int(num)],'pm10vale':pm10list[int(num)], 
+                            'so2value':so2list[int(num)], 'covalue':colist[int(num)], 'o3value':o3list[int(num)], 'no2value':no2list[int(num)], 'datatime':timelist[int(num)]})
     
     timer.start()
 
