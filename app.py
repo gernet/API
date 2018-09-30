44438fe691d78a6a979c1d207627f365b95e2ace
realsubin97 <38903619+realsubin97@users.noreply.github.com>
2018-09-30 17:47:31 +0900

    openshift

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
 import imp
+
+if __name__ == '__main__':
+    app = imp.load_source('APIfirebase','app/APIfirebase.py')
    
-def application(environ, start_response):
-    start_response('200 OK', [('APIfirebase','app/APIfirebase.py')])
+#def application(environ, start_response):
+#    start_response('200 OK', [('APIfirebase','app/APIfirebase.py')])
