4bfd2a75f87ba6a74dd48912c3561ea183892aeb
realsubin97 <38903619+realsubin97@users.noreply.github.com>
2018-09-30 18:29:24 +0900

    json file package_data

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ setup(
     version ="1.0",
     packages= find_packages(),
     install_requires = ['requests','pandas','bs4','app','firebase_admin'],
+    package_data={'realace2018-firebase-adminsdk-r7gk1-121be5edaf.json':'api/realace2018-firebase-adminsdk-r7gk1-121be5edaf.json'}
 )
