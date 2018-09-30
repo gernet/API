999278e5a8940df854f3fc56fdefd05f1deab003
realsubin97 <38903619+realsubin97@users.noreply.github.com>
2018-09-30 18:17:50 +0900

    openshift

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import imp
 
 if __name__ == '__main__':
     app = imp.load_source('APIfirebase','api/APIfirebase.py')
+    app2 = imp.load_source('realace2018-firebase-adminsdk-r7gk1-121be5edaf.json','api/realace2018-firebase-adminsdk-r7gk1-121be5edaf.json')
     
 #def application(environ, start_response):
 #    start_response('200 OK', [('APIfirebase','app/APIfirebase.py')])
